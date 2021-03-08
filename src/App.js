f69cebe439438f182352d68b52cb9d5881a41f11
dariuszb <dariusz.berer@gmail.com>
2021-03-08 21:09:16 +0100

    Header add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./components/Chat";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import styled from "styled-components";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/room">Chat Page</Route>
-          <Route path="/">Login</Route>
-        </Switch>
+        <Container>
+          <Header />
+          <Switch>
+            <Route path="/room">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div``;
