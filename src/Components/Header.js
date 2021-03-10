544ba5f9828327a06c43fc17f1e7b8fcc432d125
dariuszb <dariusz.berer@gmail.com>
2021-03-10 20:19:02 +0100

    icons add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Main>
+        <AccessTimeIcon />
+        <HelpOutlineIcon />
+      </Main>
+      {/* <UserContainer></UserContainer> */}
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div``;
+const Main = styled.div``;
