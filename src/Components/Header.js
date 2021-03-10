841171061bfdb06acfd96aaba7dfa8089184409b
dariuszb <dariusz.berer@gmail.com>
2021-03-10 20:40:24 +0100

    search input

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,14 +8,59 @@ function Header() {
     <Container>
       <Main>
         <AccessTimeIcon />
+        <SearchContainer>
+          <Search>
+            <input type="text" placeholder="Search" />
+          </Search>
+        </SearchContainer>
         <HelpOutlineIcon />
       </Main>
-      {/* <UserContainer></UserContainer> */}
+      <UserContainer>
+        <Name>Nazariy</Name>
+        <UserImage>
+          <img src="https://i.imgur.com/6VBx3io.png" />
+        </UserImage>
+      </UserContainer>
     </Container>
   );
 }
 
 export default Header;
 
-const Container = styled.div``;
-const Main = styled.div``;
+const Container = styled.div`
+  background-color: #350d36;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Main = styled.div`
+  display: flex;
+`;
+const SearchContainer = styled.div`
+  min-width: 400px;
+`;
+const Search = styled.div`
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+  border-radius: 6px;
+  width: 100%;
+  input {
+    width: 100%;
+    background-color: transparent;
+    border: none;
+    padding-left: 8px;
+    padding-right: 8px;
+    color: white;
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
+  input:focus {
+    outline: none;
+  }
+`;
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Name = styled.div``;
+const UserImage = styled.div``;
