79b2ae066170557444c33f67757deb257efb9e19
dariuszb <dariusz.berer@gmail.com>
2021-03-11 20:25:39 +0100

    sidebar message style

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 function Sidebar() {
-  return <Container>Sidebar</Container>;
+  return (
+    <Container>
+      <WorkspaceContainer>
+        <Name>ClevelProgrammer</Name>
+        <NewMessage>
+          <AddCircleOutlineIcon />
+        </NewMessage>
+      </WorkspaceContainer>
+    </Container>
+  );
 }
 
 export default Sidebar;
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: #3f0e40;
+`;
+const WorkspaceContainer = styled.div`
+  color: white;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+  justify-content: space-between;
+  border-bottom: 1px solid #532753;
+`;
+const Name = styled.div``;
+const NewMessage = styled.div`
+  width: 36px;
+  height: 36px;
+  background-color: white;
+  color: #3f0e40;
+  fill: #3f0e40;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 20px;
+  cursor: pointer;
+`;
