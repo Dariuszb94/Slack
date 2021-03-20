c723a756ee40a2872c9fb034d1b6961efd19ace1
dariuszb <dariusz.berer@gmail.com>
2021-03-20 10:53:23 +0100

    chat chanell style

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -5,7 +5,13 @@ import StarBorderIcon from "@material-ui/icons/StarBorder";
 function Chat() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <Channel>
+          <ChannelName>#clever</ChannelName>
+          <ChannelInfo>Company wide</ChannelInfo>
+        </Channel>
+        <ChannelDetails></ChannelDetails>
+      </Header>
       <MessageContainer></MessageContainer>
       <ChatInput></ChatInput>
     </Container>
@@ -17,6 +23,21 @@ const Container = styled.div`
   display: grid;
   grid-template-rows: 64px auto min-content;
 `;
-const Header = styled.div``;
+const Header = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+const Channel = styled.div``;
+const ChannelDetails = styled.div``;
+const ChannelName = styled.div`
+  font-weight: 700;
+`;
+const ChannelInfo = styled.div`
+  font-weight: 400;
+  color: #606060;
+  font-size: 13px;
+  margin-top: 8px;
+`;
+
 const MessageContainer = styled.div``;
 const ChatInput = styled.div``;
