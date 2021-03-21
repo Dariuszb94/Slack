29496a718d17c92ce06df569464f9fdd5219797d
dariuszb <dariusz.berer@gmail.com>
2021-03-21 17:39:57 +0100

    chat message avatar

--- src/Components/ChatMessage.js
+++ src/Components/ChatMessage.js
@@ -5,10 +5,12 @@ function ChatMessage() {
   return (
     <Container>
       <UserAvatar>
-        <img src="" />
+        <img src="https://randomuser.me/api/portraits/women/10.jpg" />
       </UserAvatar>
       <MessageContent>
-        <Name>Raf Qazi</Name>
+        <Name>
+          Raf Qazi<span>2/23/2021</span>
+        </Name>
         <Text>How is</Text>
       </MessageContent>
     </Container>
@@ -18,9 +20,42 @@ function ChatMessage() {
 export default ChatMessage;
 
 const Container = styled.div`
-  background-color: orange;
+  padding: 8px 20px;
+  display: flex;
+  align-items: center;
+  -webkit-transition: background-color 0.2s ease-in-out;
+  -moz-transition: background-color 0.2s ease-in-out;
+  -ms-transition: background-color 0.2s ease-in-out;
+  -o-transition: background-color 0.2s ease-in-out;
+  transition: background-color 0.2s ease-in-out;
+  :hover {
+    background-color: #f5f5f5;
+  }
 `;
-const UserAvatar = styled.div``;
-const MessageContent = styled.div``;
-const Name = styled.span``;
+const UserAvatar = styled.div`
+  width: 36px;
+  height: 36px;
+  border-radius: 2px;
+  overflow: hidden;
+  margin-right: 8px;
+  img {
+    width: 100%;
+  }
+`;
+const MessageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Name = styled.span`
+  font-weight: 900;
+  font-size: 15px;
+  line-height: 1.4;
+
+  span {
+    margin-left: 8px;
+    font-size: 13px;
+    font-weight: 400;
+    color: rgb(97, 86, 87);
+  }
+`;
 const Text = styled.span``;
