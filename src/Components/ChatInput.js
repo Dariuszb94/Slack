a9ad45b0c20a5358c1a1cdc1c667cab528f6a611
DariuszB94 <greedo904@gmail.com>
2021-03-21 10:44:57 +0100

    chatinput form

--- src/Components/ChatInput.js
+++ src/Components/ChatInput.js
@@ -1,11 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-
+import SendIcon from "@material-ui/icons/Send"
 function ChatInput() {
-  return <Container></Container>;
+  return <Container><InputContainer><form>
+    <input type="text" placeholder="Message here..."/><SendButton><SendIcon/></SendButton></form></InputContainer></Container>;
 }
 
 export default ChatInput;
 const Container = styled.div`
-  background-color: orange;
+padding-left:20px;
+padding-right:20px;
+padding-left:24px;
+
 `;
+const InputContainer = styled.div`
+border: 1px solid #8d8d8e;
+border-radius:4px;
+form{
+  display:flex;
+  height:42px;
+  align-items:center;
+  padding-left:10px;
+  input{
+    flex:1;
+    border:none;
+    font-size:13px;
+  }
+  input:focus{
+    outline:none;
+  }
+}
+`;
+const SendButton = styled.div``;
