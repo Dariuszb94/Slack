71d00c1afec7485de1de911890cfd7b057dd5fbd
dariuszb <dariusz.berer@gmail.com>
2021-03-22 19:20:24 +0100

    login content

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,9 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <Content>
+        <SlackImg src="https://img.icons8.com/ios/452/communication.png" />
+      </Content>
+    </Container>
+  );
 }
 
 export default Login;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f8f8f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Content = styled.div`
+  background-color: white;
+`;
+const SlackImg = styled.img`
+  height: 100px;
+`;
