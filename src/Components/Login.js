0f55eb82ee2fd44a31f8f36401b8c4acb1ff4b3b
dariuszb <dariusz.berer@gmail.com>
2021-04-13 21:16:58 +0200

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { auth, provider } from "../firebase";
+import { keyframes } from "styled-components";
 function Login(props) {
   const signIn = () => {
     auth
@@ -21,7 +22,7 @@ function Login(props) {
     <Container>
       <Content>
         <SlackImg src="https://img.icons8.com/ios/452/communication.png" />
-        <h1>Sign in Slack</h1>
+        <h1>Chatter</h1>
         <SignInButton
           onClick={() => {
             signIn();
@@ -39,7 +40,6 @@ export default Login;
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: #f8f8f8;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -47,24 +47,71 @@ const Container = styled.div`
 const Content = styled.div`
   background-color: white;
   padding: 100px;
-  border-radius: 5px;
-  box-shadow: 0 1px 3px rgb(0 0 0 /12%), 0 1px 2px rgb(0 0 0 / 24%);
+  border-radius: 4px;
+  -webkit-box-shadow: 10px 9px 24px 3px rgba(63, 14, 64, 0.71);
+  -moz-box-shadow: 10px 9px 24px 3px rgba(63, 14, 64, 0.71);
+  box-shadow: 10px 9px 24px 3px rgba(63, 14, 64, 0.71);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  @media (max-width: 1000px) {
+    padding: 40px;
+  }
 `;
 const SlackImg = styled.img`
   height: 100px;
   margin-bottom: 25px;
 `;
+const emerge = keyframes`
+  0% {
+    transform: translateY(100px) scale(0.3);
+    color:transparent;
+  }
+80%{
+  color:transparent;
+  transform:scale(1.2);
+
+}
+  100% {
+    transform: translateY(0) scale(1);
+    color:white;
+
+  }
+`;
+
 const SignInButton = styled.button`
+  height: 48px;
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 8px 12px rgb(0 0 0 / 10%);
+  border-radius: 4px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 18px;
   margin-top: 25px;
-  background-color: #0a8d48;
+  background-color: #3f0e40;
   color: white;
   border: none;
   height: 40px;
   border-radius: 4px;
   cursor: pointer;
   font-size: 15px;
+  -webkit-transition: transform 0.2s ease-in-out,
+    background-color 0.2s ease-in-out;
+  -moz-transition: transform 0.2s ease-in-out, background-color 0.2s ease-in-out;
+  -ms-transition: transform 0.2s ease-in-out, background-color 0.2s ease-in-out;
+  -o-transition: transform 0.2s ease-in-out, background-color 0.2s ease-in-out;
+  transition: transform 0.2s ease-in-out, background-color 0.2s ease-in-out;
+  animation: ${emerge} 1s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+    background-color: #522653;
+  }
+  @media (max-width: 1000px) {
+    width: 200px;
+  }
 `;
